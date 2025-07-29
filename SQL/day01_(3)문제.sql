944d0a372541028e558dd9d80593802bac8ee7e9
suyoung0211 <suyoung332211@gmail.com>
2025-07-29 17:08:21 +0900

    day01

---
+++ SQL/day01_(3)문제.sql
@@ -0,0 +1,26 @@
+/* 
+tbl_carco2 테이블의 데이터를 조회하는 예시를 만들어 보세요.
+(SELECT ~ FROM tbl_carco2 WHERE 조건)
+각자 7개 씩
+ */
+
+ -- 1. volume 이 2100 이상인 모든 컬럼 조회
+ SELECT * FROM TBL_CARCO2 WHERE volume >= 2100;
+
+ -- 2. co2 가 110 이상인 모든 칼럼 조회
+ SELECT * FROM TBL_CARCO2 WHERE CO2 >= 110;
+
+ -- 3. model 중 'C' 가 들어가는 모든 칼럼 조회
+ SELECT * FROM TBL_CARCO2 WHERE MOELD LIKE '%C%';
+
+ -- 4. weight 가 1100 보다 작은 모든 칼럼
+ SELECT * FROM TBL_CARCO2 WHERE WEIGHT < 1100;
+
+ -- 5. weight 가 1100 이하인 칼럼 중 co2 가 99 인 칼럼
+ SELECT * FROM TBL_CARCO2 WHERE WEIGHT <= 1100 AND CO2 = 99;
+
+ -- 6. car 가 'S' 로 시작하는 칼럼 중 volume 이 1200 보다 큰 칼럼
+ SELECT * FROM TBL_CARCO2 WHERE CAR LIKE 'S%' AND VOLUME > 1200;
+
+ -- 7. car 중 T ~ Z 사이 단어로 시작하는 칼럼
+ SELECT * FROM TBL_CARCO2 WHERE CAR BETWEEN 'T%' AND 'Z%';
