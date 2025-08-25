71398f4a8acb9be21d90e46e4a2140e7a51a530f
suyoung0211 <suyoung332211@gmail.com>
2025-08-25 17:02:31 +0900

    게시판

--- SQL/게시판_(4)useraccount.sql
+++ SQL/게시판_(4)useraccount.sql
@@ -23,3 +23,7 @@ INSERT INTO user_account (userid, username, password, birth, gender, email)
 VALUES   ('seri111','박세리','1111','1992-05-20','F','seri111@gmail.com');
 INSERT INTO user_account (userid, username, password, birth, gender, email) 
 VALUES   ('nara34','정나라','1111','1988-01-11','M','nara34@gmail.com');
+
+-- JPA 수정 : Gender 타입이 문자열로 변환할 때 char 타입은 X
+-- 컬럼 타입을 varchar2 로 변경
+ALTER TABLE user_account MODIFY gender VARCHAR2(4);
